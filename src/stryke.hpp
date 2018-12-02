63b23039c67db36e1d9cd1b144c30f9574adc8cc
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-02 12:55:39 +0100

    add elements for writer

--- src/stryke.hpp
+++ src/stryke.hpp
@@ -13,13 +13,14 @@
 #ifndef STRYKE_HPP_
 #define STRYKE_HPP_
 
-#include <string>
+#include "orc/OrcFile.hh"
 
 #include <iostream>
+#include <memory>
 #include <queue>
+#include <string>
 #include <thread>
 #include <tuple>
-
 
 namespace stryke {
 
@@ -29,11 +30,39 @@ namespace stryke {
 template <typename... Types>
 class OrcWriterImpl {
 public:
-  OrcWriterImpl(uint64_t batchSize, int batchNb_max, std::string folder, std::string prefix) {
+  OrcWriterImpl(uint64_t batchSize, int batchNb_max, std::string folder, std::string prefix) : batchSize(batchSize) {
+
+    // TODO: Trouver comment créer ce schema sans string
+    fileType = orc::Type::buildTypeFromString("struct<x:int>");
+
+    options.setStripeSize(1000);
+
+    outStream = orc::writeLocalFile(folder + "/" + prefix);
+    writer = orc::createWriter(*fileType, outStream.get(), options);
+    rowBatch = writer->createRowBatch(this->batchSize);
+
+  }
+
+  ~OrcWriterImpl() {
+    this->writer->close();
   }
 
   void write(Types... data) {
+
     std::cout << "argument length: " << sizeof...(data) << std::endl;
+    if (this->numValues < this->batchSize) {
+
+      this->data.push_back(std::make_tuple(data...));
+      ++this->numValues;
+
+    } else {
+      // TODO: Faire le remplissage via template
+      // TODO: on fait le remplissage à chaque fin de batch ou à chaque ajout de data ?
+
+      orc::StructVectorBatch* structBatch = dynamic_cast<orc::StructVectorBatch*>(rowBatch.get());
+      structBatch->numElements = numValues;
+    }
+
   }
 
   OrcWriterImpl &setSchema() {
@@ -41,6 +70,16 @@ public:
     return *this;
   }
 
+private:
+  std::unique_ptr<orc::Type> fileType;
+  orc::WriterOptions options;
+  std::unique_ptr<orc::OutputStream> outStream;
+  std::unique_ptr<orc::Writer> writer;
+  std::unique_ptr<orc::ColumnVectorBatch> rowBatch;
+
+  std::vector<std::tuple<Types...>> data; // buffer that holds a batch of rows in tuple
+  uint64_t numValues = 0;
+  uint64_t batchSize;
 };
 
 //! Writer in mutli file one thread.
@@ -55,7 +94,6 @@ public:
   void write(Types... data) {
     this->fifo.push(data...);
   }
-
 };
 
 //! Writer in mutli file separate thread.
@@ -79,6 +117,6 @@ public:
   std::thread writer_thread;
 };
 
-} // namespace orc
+} // namespace stryke
 
 #endif // !STRYKE_HPP_
