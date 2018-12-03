48d1c04c9adccb8d48add2912fd0f5ae28810a9b
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-03 09:06:56 +0100

    add file for dynamic code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Stryke
 WIP: A C++ library build on top of Orc C++ library.
 
+> The C++ Orc lib is highly dynamic. Data Schema is defined at runtime. As Orc have to be bind by dynmic language like python it's perfect. The goal of this lib is to make a more static/templated C++ lib. this code will try to make an easy to use writer and try to fix most of the structure at compilation time.
+
 ## Compilation
 
 We use [vcpkg](https://github.com/Microsoft/vcpkg) to manage dependencies
---
+++ src/main_dynamic.cpp
@@ -0,0 +1,27 @@
+//
+//  Stryke
+//
+//  https://github.com/edmBernard/Stryke
+//
+//  Created by Erwan BERNARD on 02/12/2018.
+//
+//  Copyright (c) 2018 Erwan BERNARD. All rights reserved.
+//  Distributed under the Apache License, Version 2.0. (See accompanying
+//  file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+//
+
+#include "stryke_dynamic.hpp"
+#include <iostream>
+#include <string>
+
+using namespace stryke;
+
+int main(int argc, char const *argv[]) {
+
+  OrcWriterImpl<int, int, long> my_writer(10, 100, "../data", "toto.orc");
+  for (int i = 0; i < 150; ++i) {
+    my_writer.write(1000 + i, 2000 + i+1, 3000 + i+2);
+  }
+
+  return 0;
+}
