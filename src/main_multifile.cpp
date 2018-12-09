cfffa0db2ae6a9aa34aa2d99c2854b2ef74018d4
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-09 11:02:28 +0100

    add multifile

--- src/main_multifile.cpp
+++ src/main_multifile.cpp
@@ -18,10 +18,10 @@ using namespace stryke;
 
 int main(int argc, char const *argv[]) {
 
-  OrcWriterMulti<DateNumber, Int, Long, Double> writer({"A2", "B2", "C2","D2"}, ".", "date.orc", 10, 100);
+  OrcWriterMulti<DateNumber, Int, Long, Double> writer({"A2", "B2", "C2","D2"}, ".", "date", 10, 100);
 
-  for (int i = 0; i < 3; ++i) {
-    writer.write(1000 + i, 2000 + i+1, 3000 + i+2, 11111.111111 + i/10000.);
+  for (int i = 0; i < 100; ++i) {
+    writer.write(300 * 30 + i/10., 2000 + i+1, 3000 + i+2, 11111.111111 + i/10000.);
   }
 
   return 0;
--- src/stryke_multifile.hpp
+++ src/stryke_multifile.hpp
@@ -25,26 +25,86 @@
 namespace stryke {
 
 namespace fs = std::filesystem;
+
+namespace utils {
+
+// template <typename T>
+// bool get_name(T mydate);
+
+// template<>
+// bool get_name(Date mydate);
+
+// template <>
+// bool get_name(DateNumber mydate) {
+//   time_t mytime = mydate.data * (60 * 60 * 24);
+//   auto mytm = gmtime(&mytime);
+//   std::cout << "years: " << 1900 + mytm->tm_year << std::endl;
+//   std::cout << "month: " << 1 + mytm->tm_mon << std::endl;
+//   std::cout << "days: " << mytm->tm_mday << std::endl;
+//   return true;
+// }
+
+// template<>
+// bool get_name(Timestamp mydate);
+
+// template<>
+// bool get_name(TimestampNumber mydate);
+
+} // namespace utils
+
 //! Writer in mutli file one thread.
 //!
 //!
-template <typename... Types>
+template <typename T, typename... Types>
 class OrcWriterMulti {
 public:
-  OrcWriterMulti(std::array<std::string, sizeof...(Types)> column_names, std::string root_folder, std::string prefix, uint64_t batchSize, int batchNb_max)
-      : column_names(column_names), batchSize(batchSize) {
-  }
-  void write(Types... dataT) {
-    for (auto &&i : this->writers) {
-      i.second.write(dataT...);
-    }
+  OrcWriterMulti(std::array<std::string, sizeof...(Types) + 1> column_names, std::string root_folder, std::string file_prefix, uint64_t batchSize, int batchNb_max)
+      : column_names(column_names), root_folder(root_folder), file_prefix(file_prefix), batchSize(batchSize) {
   }
 
-  std::array<std::string, sizeof...(Types)> column_names;
-  std::map<fs::path, OrcWriterImpl<Types...>> writers;
+  ~OrcWriterMulti() {
+  }
+
+  void write(T date, Types... dataT) {
+    std::string writers_path = this->get_writer(date);
+    this->writers[writers_path]->write(date, dataT...);
+  }
+
+private:
+  fs::path get_name(T &date) {
+    time_t mytime = date.data * (60 * 60 * 24);
+    auto mytm = gmtime(&mytime);
+
+    fs::path file_folder = this->root_folder / std::to_string(1900 + mytm->tm_year) / std::to_string(1 + mytm->tm_mon) / std::to_string(mytm->tm_mday);
+    std::string prefix_with_date = this->file_prefix + std::to_string(1900 + mytm->tm_year) + "-" + std::to_string(1 + mytm->tm_mon) + "-" + std::to_string(mytm->tm_mday);
+    // Add suffix to avoid file erasing
+    std::string filename = file_folder / (prefix_with_date + "-0" + ".orc");
+    {
+      int count = 0;
+      while (this->writers.count(filename) == 0 && fs::exists(filename)) {
+        filename = file_folder / (prefix_with_date + "-" + std::to_string(count) + ".orc");
+        ++count;
+      }
+    }
+    return filename;
+  }
+
+  std::string get_writer(T date) {
+    fs::path filename = get_name(date);
+    if (this->writers.count(filename) == 0) {
+      fs::create_directories(filename.parent_path());
+      this->writers[filename] = std::make_unique<OrcWriterImpl<T, Types...>>(this->column_names, this->batchSize, this->batchNb_max, filename);
+      this->counts[filename] = 0;
+    }
+    return filename;
+  }
+
+  std::array<std::string, sizeof...(Types) + 1> column_names;
+  std::map<fs::path, std::unique_ptr<OrcWriterImpl<T, Types...>>> writers;
+  std::map<fs::path, int> counts;
 
   fs::path root_folder;
-  std::string prefix;
+  std::string file_prefix;
   uint64_t batchSize;
   int batchNb_max;
 };
