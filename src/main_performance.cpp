2ee8645246ff6c4b3553a292073d8aba9ea65d02
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-10 08:17:24 +0100

    add dispatch in perf

--- src/main_performance.cpp
+++ src/main_performance.cpp
@@ -10,13 +10,14 @@
 //  file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
 //
 
+#include "stryke_dispatch.hpp"
 #include "stryke_multifile.hpp"
 #include "stryke_template.hpp"
 
 using namespace stryke;
 
 void bench_multi(int number_line) {
-    auto start1 = std::chrono::high_resolution_clock::now();
+  auto start1 = std::chrono::high_resolution_clock::now();
   {
     OrcWriterMulti<DateNumber, Int, Long, Double> writer_multi({"A2", "B2", "C2", "D2"}, ".", "date", 100000, 100);
     for (int i = 0; i < number_line; ++i) {
@@ -25,27 +26,38 @@ void bench_multi(int number_line) {
   }
   std::chrono::duration<double, std::milli> elapsed1 = std::chrono::high_resolution_clock::now() - start1;
   std::cout << std::setw(40) << std::left << "multi:  processing time : description : " << elapsed1.count() << " ms\n";
+}
 
+void bench_dispatch(int number_line) {
+  auto start1 = std::chrono::high_resolution_clock::now();
+  {
+    OrcWriterDispatch<DateNumber, Int, Long, Double> writer_dispatch({"A2", "B2", "C2", "D2"}, ".", "date", 100000, 100);
+    for (int i = 0; i < number_line; ++i) {
+      writer_dispatch.write(300 * 30 + i / 10., 2000 + i + 1, 3000 + i + 2, 11111.111111 + i / 10000.);
+    }
+  }
+  std::chrono::duration<double, std::milli> elapsed1 = std::chrono::high_resolution_clock::now() - start1;
+  std::cout << std::setw(40) << std::left << "dispatch:  processing time : description : " << elapsed1.count() << " ms\n";
 }
 
 void bench_simple(int number_line) {
-    auto start2 = std::chrono::high_resolution_clock::now();
+  auto start1 = std::chrono::high_resolution_clock::now();
   {
     OrcWriterImpl<DateNumber, Int, Long, Double> writer_simple({"A2", "B2", "C2", "D2"}, "date", 100);
     for (int i = 0; i < number_line; ++i) {
       writer_simple.write(300 * 30 + i / 10., 2000 + i + 1, 3000 + i + 2, 11111.111111 + i / 10000.);
     }
   }
-  std::chrono::duration<double, std::milli> elapsed2 = std::chrono::high_resolution_clock::now() - start2;
+  std::chrono::duration<double, std::milli> elapsed1 = std::chrono::high_resolution_clock::now() - start1;
 
-  std::cout << std::setw(40) << std::left << "simple: processing time : description : " << elapsed2.count() << " ms\n";
-
+  std::cout << std::setw(40) << std::left << "simple: processing time : description : " << elapsed1.count() << " ms\n";
 }
 
 int main(int argc, char const *argv[]) {
 
-  int number_line = 10000;
+  int number_line = 1000;
   bench_multi(number_line);
+  bench_dispatch(number_line);
   bench_simple(number_line);
 
   return 0;
