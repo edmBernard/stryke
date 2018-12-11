a0d3bfdb6f5230f5d51f557a5a6bf39dd79ee0a3
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-11 08:16:33 +0100

    add some types test

--- tests/unit-template.cpp
+++ tests/unit-template.cpp
@@ -4,7 +4,7 @@
 
 namespace fs = std::filesystem;
 
-TEST_CASE("OrcWriterImpl", "[Simple]") {
+TEST_CASE("OrcWriterImpl Types", "[Simple]") {
 
   std::string filename = "example.orc";
 
@@ -12,7 +12,40 @@ TEST_CASE("OrcWriterImpl", "[Simple]") {
     {
       stryke::OrcWriterImpl<stryke::DateNumber, stryke::Int> writer({"date", "col1"}, filename, 10000);
       for (int i = 0; i < 10; ++i) {
-        writer.write(i+1, i);
+        writer.write(i, i);
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test Short") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::Short> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, i);
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test Long") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::Long> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, i);
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test Float") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::Float> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i + 1, i / 1000.);
       }
     }
     stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
@@ -23,7 +56,51 @@ TEST_CASE("OrcWriterImpl", "[Simple]") {
     {
       stryke::OrcWriterImpl<stryke::DateNumber, stryke::Double> writer({"date", "col1"}, filename, 10000);
       for (int i = 0; i < 10; ++i) {
-        writer.write(i+1, i/1000.);
+        writer.write(i, i / 1000.);
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test Date") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::Date> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, "1990-12-18 12:26:20");
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test DateNumber") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::DateNumber> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, i+1);
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test Timestamp") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::Timestamp> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, "1990-12-18 12:26:20");
+      }
+    }
+    stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
+    REQUIRE(tmp_b.nbr_columns == 3);
+    REQUIRE(tmp_b.nbr_rows == 10);
+  }
+  SECTION("Test TimestampNumber") {
+    {
+      stryke::OrcWriterImpl<stryke::DateNumber, stryke::TimestampNumber> writer({"date", "col1"}, filename, 10000);
+      for (int i = 0; i < 10; ++i) {
+        writer.write(i, i+1);
       }
     }
     stryke::BasicStats tmp_b = stryke::get_basic_stats(filename);
