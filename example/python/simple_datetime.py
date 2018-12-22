0d0acc9aeef008e4b17bcb3f4d1f18599719b54d
Erwan BERNARD <bernard.erwan@gmail.com>
2018-12-22 17:37:08 +0100

    update python example

---
+++ example/python/simple_datetime.py
@@ -0,0 +1,38 @@
+#
+#  Stryke
+#
+#  https://github.com/edmBernard/Stryke
+#
+#  Created by Erwan BERNARD on 17/12/2018.
+#
+#  Copyright (c) 2018 Erwan BERNARD. All rights reserved.
+#  Distributed under the Apache License, Version 2.0. (See accompanying
+#  file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+#
+
+
+try:
+    # These examples assume stryke is install on your system
+    import stryke as sy
+except ModuleNotFoundError as e:
+    print("These examples assume stryke is install on your system. ==> %s" % e)
+    exit(1)
+
+import argparse
+from datetime import datetime
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Write a Orc file')
+    parser.add_argument('--filename', '-f', type=str, required=True, help='The name of the file')
+    parser.add_argument('--batch_size', type=int, default=10000, help='The batch size')
+    parser.add_argument('--nbr_row', type=int, default=100, help='The batch size')
+
+    args = parser.parse_args()
+
+    options = sy.WriterOptions()
+    options.set_batch_size(args.batch_size)
+
+    writer = sy.simple.WriterDateNPoint2l(["Date", "x", "y"], args.filename, options)
+    for i in range(args.nbr_row):
+        writer.write(datetime.now().timestamp() / (60*60*24), i, i * 2)
+
